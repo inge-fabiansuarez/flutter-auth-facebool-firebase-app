f0fe9f9f25edbb8b6a1dbc3ec6d85d22508599c4
Fabian Suarez <inge.fabiansuarez@gmail.com>
2023-11-21 13:25:42 -0500

    se agrego la autenticacion con Facebook

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +14,98 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyApp> {
+  Map<String, dynamic>? _userData;
+  bool _checking = true;
+  AccessToken? _accessToken;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkIfisLoggedIn();
+  }
+
+  _checkIfisLoggedIn() async {
+    final accessToken = await FacebookAuth.instance.accessToken;
+    print(accessToken);
+    setState(() {
+      _checking = false;
+    });
+
+    if (accessToken != null) {
+      print(accessToken.toJson());
+      final userData = await FacebookAuth.instance.getUserData();
+      _accessToken = accessToken;
+      setState(() {
+        _userData = userData;
+      });
+    } else {
+      _login();
+    }
+  }
+
+  _login() async {
+    final LoginResult result = await FacebookAuth.instance.login();
+    if (result.status == LoginStatus.success) {
+      _accessToken = result.accessToken;
+
+      final userData = await FacebookAuth.instance.getUserData();
+      _userData = userData;
+    } else {
+      print(result.status);
+      print("fabian${result.message}");
+    }
+
+    setState(() {
+      _checking = false;
+    });
+  }
+
+  _logout() async {
+    await FacebookAuth.instance.logOut();
+    _accessToken = null;
+    _userData = null;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Facebook Auth Project')),
+        body: _checking
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Center(
+                child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _userData != null
+                      ? Text('name: ${_userData!['name']}')
+                      : Container(),
+                  _userData != null
+                      ? Text('email: ${_userData!['email']}')
+                      : Container(),
+                  _userData != null
+                      ? Container(
+                          child: Image.network(
+                              _userData!['picture']['data']['url']),
+                        )
+                      : Container(),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  CupertinoButton(
+                      color: Colors.blue,
+                      child: Text(
+                        _userData != null ? 'LOGOUT' : 'LOGIN',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: _userData != null ? _logout : _login)
+                ],
+              )),
+      ),
+    );
   }
 }
