cd1201d1ee0474c0e7331fef36624211e0c5f60d
Fabian Suarez <inge.fabiansuarez@gmail.com>
2023-11-21 14:39:26 -0500

    conexion Facebook

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,130 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    runApp(MyApp());
+  } catch (e) {
+    print('Error al inicializar Firebase: $e');
+  }
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyApp> {
-  Map<String, dynamic>? _userData;
-  bool _checking = true;
-  AccessToken? _accessToken;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkIfisLoggedIn();
-  }
-
-  _checkIfisLoggedIn() async {
-    final accessToken = await FacebookAuth.instance.accessToken;
-    print(accessToken);
-    setState(() {
-      _checking = false;
-    });
-
-    if (accessToken != null) {
-      print(accessToken.toJson());
-      final userData = await FacebookAuth.instance.getUserData();
-      _accessToken = accessToken;
-      setState(() {
-        _userData = userData;
-      });
-    } else {
-      _login();
-    }
-  }
-
-  _login() async {
-    final LoginResult result = await FacebookAuth.instance.login();
-    if (result.status == LoginStatus.success) {
-      _accessToken = result.accessToken;
-
-      final userData = await FacebookAuth.instance.getUserData();
-      _userData = userData;
-    } else {
-      print(result.status);
-      print("fabian${result.message}");
-    }
-
-    setState(() {
-      _checking = false;
-    });
-  }
-
-  _logout() async {
-    await FacebookAuth.instance.logOut();
-    _accessToken = null;
-    _userData = null;
-    setState(() {});
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Facebook Auth Project')),
-        body: _checking
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : Center(
-                child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  _userData != null
-                      ? Text('name: ${_userData!['name']}')
-                      : Container(),
-                  _userData != null
-                      ? Text('email: ${_userData!['email']}')
-                      : Container(),
-                  _userData != null
-                      ? Container(
-                          child: Image.network(
-                              _userData!['picture']['data']['url']),
-                        )
-                      : Container(),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  CupertinoButton(
-                      color: Colors.blue,
-                      child: Text(
-                        _userData != null ? 'LOGOUT' : 'LOGIN',
-                        style: TextStyle(color: Colors.white),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FacebookLoginScreen(),
+    );
+  }
+}
+
+class FacebookLoginScreen extends StatefulWidget {
+  @override
+  _FacebookLoginScreenState createState() => _FacebookLoginScreenState();
+}
+
+class _FacebookLoginScreenState extends State<FacebookLoginScreen> {
+  User? _user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Facebook Login'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                LoginResult result = await FacebookAuth.instance.login();
+                AccessToken? accessToken = result.accessToken;
+
+                final AuthCredential credential =
+                    FacebookAuthProvider.credential(
+                  accessToken!.token,
+                );
+
+                try {
+                  final UserCredential userCredential =
+                      await FirebaseAuth.instance.signInWithCredential(
+                    credential,
+                  );
+                  final User? user = userCredential.user;
+
+                  setState(() {
+                    _user = user;
+                  });
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        'Inicio de sesión exitoso como ${_user?.displayName}',
                       ),
-                      onPressed: _userData != null ? _logout : _login)
+                    ),
+                  );
+                } catch (e) {
+                  print('Error al iniciar sesión con Facebook: $e');
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Error al iniciar sesión'),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              ),
+              child: Text(
+                'Iniciar sesión con Facebook',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            SizedBox(height: 20),
+            if (_user != null)
+              Column(
+                children: [
+                  Text(
+                    'Información del usuario:',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(
+                      _user?.photoURL ?? '',
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Nombre: ${_user?.displayName}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    'Correo electrónico: ${_user?.email}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    'UID: ${_user?.uid}',
+                    style: TextStyle(fontSize: 16),
+                  ),
                 ],
-              )),
+              ),
+          ],
+        ),
       ),
     );
   }
